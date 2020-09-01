bbcd2e2bd6f06ed05a0ad748e1edc9b57ce8b5cc
Houssem1980 <70571201+Houssem1980@users.noreply.github.com>
2020-09-01 10:38:52 +0200

    Add project files.

---
+++ Canibuybeer med funktion/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Canibuybeer_med_funktion
+{
+    class Program
+    {
+       
+        static bool CanIBuyBeer ( int age, string location, decimal promille, string legg)
+        {
+            if (legg != "J")
+                return false;
+                    if (promille > 1.0m)
+                return false;
+            if (age > 17 && location == "krogen")
+                return true;
+            if (age > 19 && location == "systemet")
+                return true;
+            return false;
+        }
+
+    }
+}
